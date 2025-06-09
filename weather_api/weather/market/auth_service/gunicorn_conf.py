952b027d6e7b2c42b9f33c61c720ce95d68dbe1f
Matosyan Artur <matos85@mail.ru>
2025-06-09 22:38:01 +0300

    Удалил лишние файлы и папки

--- weather_api/weather/market/auth_service/gunicorn_conf.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-
-workers = multiprocessing.cpu_count() * 2 + 1  # Количество воркеров
-bind = "0.0.0.0:8000"
-worker_class = "uvicorn.workers.UvicornWorker"
-timeout = 120
-accesslog = "-"
-errorlog = "-"
-loglevel = "info"
